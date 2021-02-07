6f849505c86756fd472c3b3fa165d029ce345d93
raimieltan <54674698+raimieltan@users.noreply.github.com>
2021-02-07 22:58:53 +0800

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
-import { toast } from "react-toastify"
+import { toast } from "react-toastify mamon"
 
 import "./css/login.css"
 
